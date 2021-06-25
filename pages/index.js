06c8d6f8220ad167b57e07a594db1bc6c505ee17
dallonkang <dallonkang@hotmail.com>
2021-06-25 17:14:27 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,11 +21,11 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
+        <p>Hi, this is Dallon. I'm a beginning Full Stack Developer. You can contact me through my <a href="mailto:dallonkang@hotmail.com">email</a>.</p>
+        {/* <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        </p> */}
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
